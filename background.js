d0ad9a4e4c8b72878dc96051e467154e53b299e7
Kaibu <notkaibu@gmail.com>
2023-02-04 01:11:35 +0100

    rebrand

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 openPanelID = function (id) {
-  chrome.tabs.create({url: 'https://support.realliferpg.de/db/player/' + id})
+  chrome.tabs.create({url: 'https://support.panthor.de/db/player/' + id})
 }
 
 chrome.runtime.onInstalled.addListener(function () {
@@ -10,33 +10,8 @@ chrome.runtime.onInstalled.addListener(function () {
   })
 })
 
-chrome.contextMenus.create({
-  id: 'open-panel',
-  title: 'Panel öffnen',
-  contexts: ['selection']
-})
-
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === 'open-panel') {
     openPanelID(info.selectionText)
   }
 })
-
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    console.log(request, sender)
-    switch (request.schema) {
-      case 'phab':
-        $.ajax({
-          type: request.type,
-          url: request.url,
-          data: request.data,
-          success: function (data) {
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-              chrome.tabs.sendMessage(tabs[0].id, {action: 'phab-data', data: data}, function (response) {})
-            })
-          }
-        })
-    }
-  })
-
--- profile.js
+++ profile.js
@@ -1,11 +1,16 @@
 var button = document.createElement("a");
 var span = document.createElement("span");
-var text = document.createTextNode("R")
-span.appendChild(text);
+var img = document.createElement("img")
+span.appendChild(img);
 button.appendChild(span);
-button.href = "https://support.realliferpg.de/steamre" + window.location.pathname;
-span.className = "rl-button";
+button.href = "https://support.panthor.de/steamre" + window.location.pathname
 button.className = "btn_profile_action btn_medium";
+button.target = "_blank"
+span.className = "panthor-button";
+img.src = "https://static.panthor.de/img/brand/icon128.png"
+img.className = "reward_btn_icon"
+img.style.width="20px"
+img.style.height="auto"
 buttons = document.getElementsByClassName("profile_header_actions")[0];
 buttons.appendChild(button)
 
